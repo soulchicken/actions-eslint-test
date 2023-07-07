a67e8066e93663020a67fcf83b62f0238e873906
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-07 22:59:23 +0900

    ESLint 억까 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const num = 100;
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>
-      Hello Next.js 👋 {num}
+      Hello Next.js 👋{num}
     </h1>
     <p>
       <Link href="/about">About</Link>
